506239e2788371e1432b4db216d64574bcd489e9
DESKTOP-VEPRU35\김성민 <zcvbnm90@naver.com>
2021-12-20 12:20:43 +0900

    Pageable을 이용한 List를 위해 메서드 추가

--- src/main/java/com/egu/boot/BoardGame/model/api/ListResult.java
+++ src/main/java/com/egu/boot/BoardGame/model/api/ListResult.java
@@ -2,10 +2,14 @@ package com.egu.boot.BoardGame.model.api;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+
 import lombok.Data;
 
 @Data
 public class ListResult<T> extends CommonResult {
  
 	private List<T> list;
+	
+	private Page<T> pageList;
 }
--- src/main/java/com/egu/boot/BoardGame/service/api/ResponseService.java
+++ src/main/java/com/egu/boot/BoardGame/service/api/ResponseService.java
@@ -2,6 +2,7 @@ package com.egu.boot.BoardGame.service.api;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 
 import com.egu.boot.BoardGame.model.api.CommonResult;
@@ -14,55 +15,62 @@ import lombok.Getter;
 public class ResponseService {
 
 	@Getter
-	public enum ResponseEnum{
-		SUCCESS(0, "성공하였습니다."),
-		FAIL(-1, "실패하였습니다.");
-		
+	public enum ResponseEnum {
+		SUCCESS(0, "성공하였습니다."), FAIL(-1, "실패하였습니다.");
+
 		private int code;
 		private String msg;
-		
+
 		private ResponseEnum(int code, String msg) {
 			this.code = code;
 			this.msg = msg;
 		}
 	}
-	
-	//결과 모델에 api 요청 성공 데이터를 셋팅해주는 api
-	private void setSuccessResult(CommonResult result) {
+
+	// 결과 모델에 api 요청 성공 데이터를 셋팅해주는 api
+	public void setSuccessResult(CommonResult result) {
 		result.setSuccess(true);
 		result.setCode(ResponseEnum.SUCCESS.getCode());
 		result.setMassage(ResponseEnum.SUCCESS.getMsg());
 	}
-	
-	//성공 결과를 리턴하는 메소드
-	private void getSuccessResult() {
+
+	// 성공 결과를 리턴하는 메소드
+	public CommonResult getSuccessResult() {
 		CommonResult result = new CommonResult();
 		setSuccessResult(result);
+		return result;
 	}
-	
-	//실패 결과를 리턴하는 메소드
-	private void getFailResult(CommonResult result) {
+
+	// 실패 결과를 리턴하는 메소드
+	public CommonResult getFailResult(CommonResult result) {
 		result.setSuccess(false);
 		result.setCode(ResponseEnum.FAIL.getCode());
 		result.setMassage(ResponseEnum.FAIL.getMsg());
+		return result;
 	}
-	
-	//단일 결과 처리하는 메소드
-	public <T> SingleResult<T> getSingleResult(T data){
+
+	// 단일 결과 처리하는 메소드
+	public <T> SingleResult<T> getSingleResult(T data) {
 		SingleResult<T> result = new SingleResult<>();
 		result.setData(data);
 		setSuccessResult(result);
 		return result;
 	}
-	
-	//복수 결과 처리하는 메소드
-	public <T> ListResult<T> getListResult(List<T> data){
+
+	// 복수 결과 처리하는 메소드 - 리스트
+	public <T> ListResult<T> getListResult(List<T> data) {
 		ListResult<T> result = new ListResult<>();
 		result.setList(data);
 		setSuccessResult(result);
 		return result;
 	}
-	
-	
-	
+
+	// 복수 결과 처리하는 메소드 - 페이지
+	public <T> ListResult<T> getPageListResult(Page<T> data) {
+		ListResult<T> result = new ListResult<>();
+		result.setPageList(data);
+		setSuccessResult(result);
+		return result;
+	}
+
 }
