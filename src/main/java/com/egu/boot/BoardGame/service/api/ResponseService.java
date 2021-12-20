9efa60b1521f33b4fade86fe8e16f4f4e58e56cc
DESKTOP-VEPRU35\김성민 <zcvbnm90@naver.com>
2021-12-20 11:28:36 +0900

    ResponseService의 메소드 추가와 성공 처리 메서드 수정

--- src/main/java/com/egu/boot/BoardGame/service/api/ResponseService.java
+++ src/main/java/com/egu/boot/BoardGame/service/api/ResponseService.java
@@ -2,12 +2,15 @@ package com.egu.boot.BoardGame.service.api;
 
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
 import com.egu.boot.BoardGame.model.api.CommonResult;
 import com.egu.boot.BoardGame.model.api.ListResult;
 import com.egu.boot.BoardGame.model.api.SingleResult;
 
 import lombok.Getter;
 
+@Service
 public class ResponseService {
 
 	@Getter
@@ -24,27 +27,31 @@ public class ResponseService {
 		}
 	}
 	
-	//성공 처리 메소드
-	private void getSuccessResult(CommonResult result) {
+	//결과 모델에 api 요청 성공 데이터를 셋팅해주는 api
+	private void setSuccessResult(CommonResult result) {
 		result.setSuccess(true);
 		result.setCode(ResponseEnum.SUCCESS.getCode());
 		result.setMassage(ResponseEnum.SUCCESS.getMsg());
 	}
 	
-	//실패 처리 메소드
+	//성공 결과를 리턴하는 메소드
+	private void getSuccessResult() {
+		CommonResult result = new CommonResult();
+		setSuccessResult(result);
+	}
+	
+	//실패 결과를 리턴하는 메소드
 	private void getFailResult(CommonResult result) {
 		result.setSuccess(false);
 		result.setCode(ResponseEnum.FAIL.getCode());
 		result.setMassage(ResponseEnum.FAIL.getMsg());
 	}
 	
-	
 	//단일 결과 처리하는 메소드
 	public <T> SingleResult<T> getSingleResult(T data){
 		SingleResult<T> result = new SingleResult<>();
 		result.setData(data);
-		getSuccessResult(result);
-	
+		setSuccessResult(result);
 		return result;
 	}
 	
@@ -52,7 +59,7 @@ public class ResponseService {
 	public <T> ListResult<T> getListResult(List<T> data){
 		ListResult<T> result = new ListResult<>();
 		result.setList(data);
-		getSuccessResult(result);
+		setSuccessResult(result);
 		return result;
 	}
 	
