9f3b68001c947df027ef2619006a9ecd4cddc7e8
DESKTOP-VEPRU35\김성민 <zcvbnm90@naver.com>
2021-12-21 17:33:30 +0900

    회원 예약 등록 컨트롤러 메서드 추가

--- src/main/java/com/egu/boot/BoardGame/controller/api/ReservationApiController.java
+++ src/main/java/com/egu/boot/BoardGame/controller/api/ReservationApiController.java
@@ -1,21 +1,33 @@
 package com.egu.boot.BoardGame.controller.api;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.egu.boot.BoardGame.model.Reservation;
+import com.egu.boot.BoardGame.model.ReservationRequestDto;
+import com.egu.boot.BoardGame.model.api.CommonResult;
+import com.egu.boot.BoardGame.repository.ReservationRepository;
+import com.egu.boot.BoardGame.service.reservationService;
+import com.egu.boot.BoardGame.service.api.ResponseService;
 
 @RestController
 public class ReservationApiController {
 
-	public void saveReservation() {
-		//필요한 거
-		//id - 자동생성
-		//인원수
-		//슬롯 id
-		//예약한 날짜 - 자동생성
-		//회원 정보 혹은 비회원 정보
-		//결제할 요금
-		
+	@Autowired
+	ResponseService responseService;
 	
+	@Autowired
+	reservationService reservationService;
+	
+	//회원이 예약 등록
+	@PostMapping("/reservation")
+	public CommonResult saveReservation(@RequestBody ReservationRequestDto reservationRequestDto) {
+		Reservation reservation =  reservationService.예약등록(reservationRequestDto);
+		return responseService.getSingleResult(reservation);
 	}
 	
+	//비회원이 예약 등록
 	
 }
