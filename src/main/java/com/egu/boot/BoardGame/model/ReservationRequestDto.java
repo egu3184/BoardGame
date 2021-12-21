82652a4a973b7f33f729c63a9d3ee77e95d6b8d9
DESKTOP-VEPRU35\김성민 <zcvbnm90@naver.com>
2021-12-21 18:20:39 +0900

    회원정보를 null처리 하기 위해 userId의 자료형을 Integer로 변경 &  비회원 이름 필드명을 guestName으로 변경

--- src/main/java/com/egu/boot/BoardGame/model/ReservationRequestDto.java
+++ src/main/java/com/egu/boot/BoardGame/model/ReservationRequestDto.java
@@ -13,13 +13,13 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class ReservationRequestDto {
 	
-	private int userId;
+	private Integer userId;
 	private int slotId;
 	private int persons;
 	private int payment;
 	
 	//비회원 예약 요청시 필요한 컬럼
-	private String name;
+	private String guestName;
 	//private String password; 예약확인용 비밀번호
 	private String phoneNumber;
 	
