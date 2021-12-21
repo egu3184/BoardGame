5500d3478163f042a28b54a2b3405548521f0e43
DESKTOP-VEPRU35\김성민 <zcvbnm90@naver.com>
2021-12-21 16:17:36 +0900

    reservationRepository 인터페이스 추가

---
+++ src/main/java/com/egu/boot/BoardGame/repository/ReservationRepository.java
@@ -0,0 +1,9 @@
+package com.egu.boot.BoardGame.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.egu.boot.BoardGame.model.Reservation;
+
+public interface ReservationRepository extends JpaRepository<Reservation, Integer> {
+
+}
