0df9c6fd3274231620078cad440b342ae58bd663
DESKTOP-VEPRU35\김성민 <zcvbnm90@naver.com>
2021-12-21 18:35:08 +0900

    reservation에 생성자 추가

--- src/main/java/com/egu/boot/BoardGame/model/Reservation.java
+++ src/main/java/com/egu/boot/BoardGame/model/Reservation.java
@@ -15,12 +15,16 @@ import javax.persistence.OneToOne;
 
 import org.hibernate.annotations.CreationTimestamp;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Entity
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Reservation {
 
 	@Id
