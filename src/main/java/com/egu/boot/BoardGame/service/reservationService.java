08cedbef8049a84db584d1ba8f21163e6e9cb795
DESKTOP-VEPRU35\김성민 <zcvbnm90@naver.com>
2021-12-22 10:39:10 +0900

    예약 등록 직전에 예약된 슬롯인지 다시 한번 체크

--- src/main/java/com/egu/boot/BoardGame/service/reservationService.java
+++ src/main/java/com/egu/boot/BoardGame/service/reservationService.java
@@ -20,51 +20,42 @@ public class reservationService {
 
 	@Autowired
 	ReservationRepository reservationRepository;
-	
+
 	@Autowired
 	UserRepository userRepository;
-	
+
 	@Autowired
 	SlotRepository slotRepository;
-	
+
 	@Transactional
 	public Reservation 예약등록(ReservationRequestDto reservationRequestDto) {
 		User user = null;
 		String phoneNumber = null;
 		String guestName = null;
-		
-		if(reservationRequestDto.getUserId() == null) {		//비회원시
+
+		if (reservationRequestDto.getUserId() == null) { // 비회원시
 			guestName = reservationRequestDto.getGuestName();
 			phoneNumber = reservationRequestDto.getPhoneNumber();
-		}else {	//회원시
-			user = userRepository.findById(reservationRequestDto.getUserId()).orElseThrow(()->{
+		} else { // 회원시
+			user = userRepository.findById(reservationRequestDto.getUserId()).orElseThrow(() -> {
 				return new IllegalArgumentException("등록된 유저가 아닙니다.");
 			});
 			guestName = user.getUsername();
 			phoneNumber = user.getPhoneNumber();
 		}
-		Slot slot = slotRepository.findById(reservationRequestDto.getSlotId()).orElseThrow(()->{
-			return new IllegalArgumentException("존재하지 않거나 예약 가능한 슬롯이 아닙니다.");
+		Slot slot = slotRepository.findById(reservationRequestDto.getSlotId()).orElseThrow(() -> {
+			throw new IllegalArgumentException("존재하지 않거나 예약 가능한 슬롯이 아닙니다.");
 		});
-		Reservation reservation = Reservation.builder()
-				.reservationTime(LocalDateTime.now())
-				.persons(reservationRequestDto.getPersons())
-				.payment(reservationRequestDto.getPayment())
-				.isPaid(false)
-				.user(user)
-				.guestName(guestName)
-				.phoneNumber(phoneNumber)
-				.slot(slot)
-				.build();
-		slot.setReserved(true);	//슬롯 예약됨으로 변경
-		return reservationRepository.save(reservation);
-	}
+		if (slot.isReserved() == true) {
+			throw new IllegalArgumentException("이미 예약된 슬롯입니다.");
+		}else {
+			Reservation reservation = Reservation.builder().reservationTime(LocalDateTime.now())
+					.persons(reservationRequestDto.getPersons()).payment(reservationRequestDto.getPayment()).isPaid(false)
+					.user(user).guestName(guestName).phoneNumber(phoneNumber).slot(slot).build();
+			slot.setReserved(true); // 슬롯 예약됨으로 변경
+			return reservationRepository.save(reservation);
+		}	
 
-	
-		
-	
-	
-	
-	
+	}
 
 }
