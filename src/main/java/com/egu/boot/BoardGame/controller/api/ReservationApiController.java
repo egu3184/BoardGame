f664ab338a0be5d341e1e180bdef3edea3ffbc9c
DESKTOP-VEPRU35\김성민 <zcvbnm90@naver.com>
2021-12-22 18:07:01 +0900

    클래스 이름 수정, 예약 조회 메서드 추가

--- src/main/java/com/egu/boot/BoardGame/controller/api/ReservationApiController.java
+++ src/main/java/com/egu/boot/BoardGame/controller/api/ReservationApiController.java
@@ -1,15 +1,16 @@
 package com.egu.boot.BoardGame.controller.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.egu.boot.BoardGame.model.Reservation;
-import com.egu.boot.BoardGame.model.ReservationRequestDto;
 import com.egu.boot.BoardGame.model.api.CommonResult;
+import com.egu.boot.BoardGame.model.dto.ReservationRequestDto;
 import com.egu.boot.BoardGame.repository.ReservationRepository;
-import com.egu.boot.BoardGame.service.reservationService;
+import com.egu.boot.BoardGame.service.ReservationService;
 import com.egu.boot.BoardGame.service.api.ResponseService;
 
 @RestController
@@ -19,17 +20,21 @@ public class ReservationApiController {
 	ResponseService responseService;
 	
 	@Autowired
-	reservationService reservationService;
+	ReservationService ReservationService;
 	
 	//회원, 비회원 예약
 	@PostMapping("/reservation")
 	public CommonResult saveReservation(@RequestBody ReservationRequestDto reservationRequestDto) {
-		Reservation reservation =  reservationService.예약등록(reservationRequestDto);
+		Reservation reservation =  ReservationService.예약등록(reservationRequestDto);
 		return responseService.getSingleResult(reservation);
 	}
 	
 	//예약 조회
-	
+	@GetMapping("/reservation/{id}")
+	public CommonResult findReservation(@RequestBody ReservationRequestDto reservationRequestDto ) {
+		Reservation reservation =  ReservationService.예약조회(reservationRequestDto);
+		return responseService.getSingleResult(reservation);
+	}
 	
 	
 	
