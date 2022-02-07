104488b26d51ea2c5478355d32d8455a1ca866de
DESKTOP-VEPRU35\김성민 <zcvbnm90@naver.com>
2022-02-07 20:01:32 +0900

    themename으로 theme 찾기 추가

--- src/main/java/com/egu/boot/BoardGame/repository/ThemeRepository.java
+++ src/main/java/com/egu/boot/BoardGame/repository/ThemeRepository.java
@@ -1,9 +1,13 @@
 package com.egu.boot.BoardGame.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.egu.boot.BoardGame.model.Theme;
 
 public interface ThemeRepository extends JpaRepository<Theme, Integer> {
 
+	Optional<Theme> findByThemeName(String themeName);	
+	
 }
