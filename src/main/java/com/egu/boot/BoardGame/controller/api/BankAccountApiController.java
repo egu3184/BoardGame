8b1135e4653cbc8c17657debac7f07541b16805b
DESKTOP-VEPRU35\김성민 <zcvbnm90@naver.com>
2022-03-21 19:05:45 +0900

    + 은행계좌 Api 컨트롤러 추가

---
+++ src/main/java/com/egu/boot/BoardGame/controller/api/BankAccountApiController.java
@@ -0,0 +1,33 @@
+package com.egu.boot.BoardGame.controller.api;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.egu.boot.BoardGame.model.api.ListResult;
+import com.egu.boot.BoardGame.model.dto.BankAccountDto;
+import com.egu.boot.BoardGame.model.dto.BankAccountDto.BankAccountResponseDto;
+import com.egu.boot.BoardGame.service.BankAccountService;
+import com.egu.boot.BoardGame.service.api.ResponseService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+public class BankAccountApiController {
+
+	private final BankAccountService baService;
+	private final ResponseService responseService;
+	
+	@GetMapping("/bank")
+	public ListResult<BankAccountResponseDto> getBAList(@RequestParam(value="branchId")int branchId){
+		
+		List<BankAccountResponseDto> list = baService.계좌리스트조회(branchId);
+		
+		return responseService.getListResult(list);
+	}
+	
+	
+}
