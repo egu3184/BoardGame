d3976ffa11da1d57f4622b7aa53b4550ddf8dd15
DESKTOP-VEPRU35\김성민 <zcvbnm90@naver.com>
2022-04-06 15:23:51 +0900

    CORS 오류 해결을 위해 기존의 WebMvcConfigurer 방식을 폐기, CORS 필터를 bean으로 만들어두고 SecurityConfig에 addFilter로 추가

---
+++ src/main/java/com/egu/boot/BoardGame/config/CorsConfigFilter.java
@@ -0,0 +1,38 @@
+package com.egu.boot.BoardGame.config;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfigFilter{
+
+	@Bean
+	public CorsFilter filter(){
+		System.out.println("cors 필터 거쳐감");
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.addAllowedHeader("*");
+		config.addAllowedMethod("*");
+		config.addAllowedOriginPattern("*");
+		
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", config);
+		
+		return new CorsFilter(source);
+	}
+	
+	
+}
--- src/main/java/com/egu/boot/BoardGame/config/security/SecurityConfig.java
+++ src/main/java/com/egu/boot/BoardGame/config/security/SecurityConfig.java
@@ -4,12 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import com.egu.boot.BoardGame.config.CorsConfigFilter;
 import com.egu.boot.BoardGame.handler.CustomJwtExceptionHandler;
 import com.egu.boot.BoardGame.service.CustomOauth2UserService;
 
@@ -18,16 +21,21 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	private final JwtTokenProvider jwtTokenProvider;
+	private final CorsConfigFilter corsfilter;
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
-			.cors().disable()
+			.addFilter(corsfilter.filter())
+//			.cors().disable()
 			.httpBasic().disable() // header에 (base64로 인코딩된) id와 pw를 받아 인증하는 방식.
 			.csrf().disable()	//rest api로 csrf 보안 필요없음.
+			//@PreAuthorize, @Secured 같은 어노테이션으로 권한 관리 가능 -> BUT 시큐리티 필터에서 예외처리 X, 사용 안하기로 
+//			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)  
 			.authorizeRequests()
 				.antMatchers("/user/**").authenticated()	 // /user/가 붙으면 인증된 유저만
 				.antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')") // /admin/이 붙으면 관리자만
@@ -41,7 +49,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 				//Jwt Token필터를 id/password 인증 필터 전에 넣음.
 			.and()	
 				.addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
-				
+			
 	}
 	
 	
