457f6e4a0ffdea984e749b9893b83b16bc453cdc
DESKTOP-VEPRU35\김성민 <zcvbnm90@naver.com>
2022-04-15 15:22:52 +0900

    불필요한 테스트 클래스 삭제

--- src/main/java/com/egu/boot/BoardGame/model/dto/UserDto.java
+++ src/main/java/com/egu/boot/BoardGame/model/dto/UserDto.java
@@ -3,6 +3,7 @@ package com.egu.boot.BoardGame.model.dto;
 import com.egu.boot.BoardGame.model.User;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 public class UserDto {
 
@@ -16,10 +17,20 @@ public class UserDto {
 	}
 	
 	@Data
+	
 	public static class UserResponseDto{
 		private String userId;
 		private String username;
+		
+		private String accessToken;
+		private String refreshToken;
 		//추후 추가
+		
+		
+		public UserResponseDto(String accessToken, String refreshToken) {
+			this.accessToken = accessToken;
+			this.refreshToken = refreshToken;
+		}
 		
 		public UserResponseDto(User user){
 			this.userId = user.getUserId();
