f2f0a30fda3ea0b34a10a1749f611c961fb2c9ef
DESKTOP-VEPRU35\김성민 <zcvbnm90@naver.com>
2022-06-13 13:56:52 +0900

    ReservationApiContoller에서 id로 예약 조회 삭제, 예약검색리스트조회(임시코드) 삭제 / 회원예약조회, 비회원예약조회 api controller method 추가 /

--- src/main/java/com/egu/boot/BoardGame/controller/api/ReservationApiController.java
+++ src/main/java/com/egu/boot/BoardGame/controller/api/ReservationApiController.java
@@ -1,16 +1,26 @@
 package com.egu.boot.BoardGame.controller.api;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.egu.boot.BoardGame.handler.CustomException;
+import com.egu.boot.BoardGame.handler.ErrorCode;
 import com.egu.boot.BoardGame.model.Reservation;
+import com.egu.boot.BoardGame.model.User;
 import com.egu.boot.BoardGame.model.api.CommonResult;
 import com.egu.boot.BoardGame.model.api.SingleResult;
 import com.egu.boot.BoardGame.model.dto.ReservationDto;
@@ -29,41 +39,33 @@ public class ReservationApiController {
 	private final ResponseService responseService;
 	private final ReservationService ReservationService;
 
-	// 예약
+	// 예약 등록
 	@PostMapping("/reservations")
 	public SingleResult<ReservationResponseDto> saveReservation(@RequestBody ReservationRequestDto reservationDto) {
 		ReservationResponseDto dto = ReservationService.예약등록(reservationDto);
 		return responseService.getSingleResult(dto);
 	}
 	
-	
-	// id로 예약 조회(추구 권한 넣을 예정)
-	@GetMapping("/reservations/{id}")
-	public SingleResult<ReservationResponseDto> findReservation(@PathVariable int id) {
-		ReservationResponseDto dto = ReservationService.예약조회(id);
+	//비회원 예약 조회
+	@GetMapping("/reservations/")
+	public SingleResult<ReservationResponseDto> getReservation(@RequestParam Integer reservationNumber ) {
+		ReservationResponseDto dto = ReservationService.비회원예약조회(reservationNumber);
 		return responseService.getSingleResult(dto);
 	}
 	
-	//예약 번호로 예약 조회
+	//회원 예약 조회
 	@GetMapping("/reservations")
-	public SingleResult<ReservationResponseDto> getReservation(
-			@RequestParam(value="reservationNumber") String reservationNum){
-		ReservationResponseDto dto = ReservationService.예약번호조회(reservationNum);
-		return responseService.getSingleResult(dto);
+	public SingleResult<Map<String, Object>> getReservation(Pageable pageable){
+		Map<String, Object> map = ReservationService.회원예약조회(pageable);
+		return responseService.getSingleResult(map);
+	}
+	
+	//회원 예약 수정
+	@PutMapping("/reservations")
+	public CommonResult updateUserReservation(@RequestBody ReservationRequestDto requestDto) {
+		long result = ReservationService.예약수정(requestDto);
+		return result > 0 ? responseService.getSuccessResult() : responseService.getFailResult(ErrorCode.RESERVATION_UPDATE_FAIL);
 	}
 
-	
-	
-	
-	
-	// 예약 검색 리스트 조회 (예약자 이름, 전화번호, 혹은 id)
-//	@GetMapping("/reservations")
-//	public CommonResult findReservation(
-//			@RequestParam(value = "bookerName", required = false) String bookerName,
-//			@RequestParam(value = "phoneNumber", required = false) String phoneNumber,
-//			@RequestParam(value="id", required = false) Integer id) {
-//		List<Reservation> reservation = ReservationService.예약검색조회(bookerName, phoneNumber,id);
-//		return responseService.getListResult(reservation);
-//	}
 
 }
