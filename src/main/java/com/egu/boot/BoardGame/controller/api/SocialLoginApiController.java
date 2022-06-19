cb6780d4b3dedd72fa934b36fbdb4699d6812b9a
DESKTOP-VEPRU35\김성민 <zcvbnm90@naver.com>
2022-06-19 18:29:18 +0900

    SocialLoginApiController 소셜회원탈퇴 api controller method 추가 , 소셜로그인 매핑 uri 수정 /

--- src/main/java/com/egu/boot/BoardGame/controller/api/SocialLoginApiController.java
+++ src/main/java/com/egu/boot/BoardGame/controller/api/SocialLoginApiController.java
@@ -1,11 +1,13 @@
 package com.egu.boot.BoardGame.controller.api;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -34,11 +36,9 @@ public class SocialLoginApiController {
 	private final ResponseService responseService;
 	private final SocialLoginService socialLoginService;
 
-	@PostMapping("/social/login/{social}")
-	public SingleResult<UserResponseDto> dd(
+	@PostMapping("/users/{social}")
+	public SingleResult<UserResponseDto> socialLogin(
 			@RequestBody TokenRequestDto dto, @PathVariable String social ){
-		System.out.println(dto.getAccessToken());
-		System.out.println(social);
 		//응답 객체 초기화
 		UserResponseDto userResponseDto = null;
 		//소셜별 처리
@@ -51,11 +51,17 @@ public class SocialLoginApiController {
 			NaverDto userProfile = socialLoginService.getNaverProfile(dto.getAccessToken());
 			userResponseDto = socialLoginService.naverLogin(userProfile);
 		}else if(social.equals("google")) {
+			//구글
 			userResponseDto = socialLoginService.googleLogin(dto.getAccessToken());
 		}
 		return responseService.getSingleResult(userResponseDto);
 	}
 	
-	
+	@DeleteMapping("/users/{social}")
+	public CommonResult deactivateSocialUser(
+			@PathVariable String social, @RequestBody TokenRequestDto requestDto) {
+		long result = socialLoginService.소셜회원탈퇴(social, requestDto);
+		return result > 0? responseService.getSuccessResult() : responseService.getFailResult(ErrorCode.COMMUNICATION_ERROR);
+	}
 	
 }
